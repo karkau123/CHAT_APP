90f99799616ae92b7551159dd7f70ad6de221f47
Kartikeya Singh <105882265+karkau123@users.noreply.github.com>
2023-12-22 04:31:19 +0530

    Adding a logout Button

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -7,11 +8,21 @@ class ChatScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter Chat'),
+        title: const Text('Flutter Chat'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              icon: Icon(
+                Icons.exit_to_app,
+                color: Theme.of(context).colorScheme.primary,
+              )),
+        ],
       ),
       body: const Center(
         child: Text('Logged in '),
-      ) ,
+      ),
     );
   }
 }
