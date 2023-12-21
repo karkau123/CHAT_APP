9a0e73e4aa911da857c8a3cfcc2235c63d1534e6
Kartikeya Singh <105882265+karkau123@users.noreply.github.com>
2023-12-21 17:11:53 +0530

    buttons added for form validtion

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class AuthScreen extends StatefulWidget {
@@ -9,63 +8,82 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  var _isLogin = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       backgroundColor: Theme.of(context).colorScheme.primary,
-       body:   Center(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Center(
           child: SingleChildScrollView(
-             child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                     Container(
-                    margin:const  EdgeInsets.only(
-                      top: 30,
-                      bottom: 20,
-                      left: 20,
-                      right: 20,
-                    ) ,
-                    width: 200,
-                    child:  Image.asset('assets/images/chat.png'),
-
-                     ),
-                     Card(
-                      
-                        margin: const  EdgeInsets.all(20),
-                        child: SingleChildScrollView(
-                            child: Padding(
-                              padding: EdgeInsets.all(16),
-                              child: Form(
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                   children: [
-                                    TextFormField(
-                                      decoration:const  InputDecoration(
-                                        labelText: 'EmailAddress', 
-                                      ),
-                                      keyboardType:TextInputType.emailAddress ,
-                                      autocorrect: false,
-                                      textCapitalization: TextCapitalization.none,
-                                    ),
-                                    TextFormField(
-                                      decoration:const  InputDecoration(
-                                        labelText: 'Password', 
-                                      ),                       
-                                 obscureText: true,
-                                    )
-                                   ],
-                                ),
-                              ),
-                            ),
-
-                            
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(
+                top: 30,
+                bottom: 20,
+                left: 20,
+                right: 20,
+              ),
+              width: 200,
+              child: Image.asset('assets/images/chat.png'),
+            ),
+            Card(
+              margin: const EdgeInsets.all(20),
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Form(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: 'EmailAddress',
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                          autocorrect: false,
+                          textCapitalization: TextCapitalization.none,
                         ),
-                     )
-                  ],
-             ),
-          )
-
-       ),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: 'Password',
+                          ),
+                          obscureText: true,
+                        ),
+                        const SizedBox(
+                          height: 12,
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor:  Theme.of(context).colorScheme.primaryContainer,
+                          ),
+                            onPressed: () {
+                              setState(() {
+                                _isLogin = !_isLogin;
+                              });
+                            },
+                            child: Text(_isLogin ? 'Login' : 'Sign up')),
+                        TextButton(
+                          onPressed: () {
+                            setState(() {
+                              _isLogin = !_isLogin;
+                            });
+                          },
+                          child: Text(_isLogin
+                              ? 'Create an Account '
+                              : 'I already have an Account'),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      )),
     );
   }
 }
